784025d128c6b7a0c011a1c90e446240da07b2ae
Timur Turianytsia <turianytsia.timur@gmail.com>
2023-02-20 12:55:43 +0200

    Adding default project

---
+++ src/main/java/com/timuranderik/customanimelist/CustomAnimeListApplication.java
@@ -0,0 +1,13 @@
+package com.timuranderik.customanimelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnimeListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAnimeListApplication.class, args);
+	}
+
+}
